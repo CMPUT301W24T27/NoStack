56acffe46aecde4a523e8174e2074fe9afea4e30
natewu <36091727+natewu@users.noreply.github.com>
2024-02-26 01:50:14 -0700

    [33] Firestore

--- app/src/main/java/com/example/nostack/MainActivity.java
+++ app/src/main/java/com/example/nostack/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.nostack;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private CollectionReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        userRef = db.collection("users");
+
+        userRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Snackbar.make(findViewById(android.R.id.content), "No users found", Snackbar.LENGTH_LONG).show();
+                    Log.e("MainActivity", "onEvent: ", error);
+                    return;
+                }
+
+                for (QueryDocumentSnapshot doc : value) {
+                    if (doc.get("name") != null) {
+                        Snackbar.make(findViewById(android.R.id.content), "Name: " + doc.get("name"), Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
     }
 }
