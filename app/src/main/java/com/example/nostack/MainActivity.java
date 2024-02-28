b3a65bc86dc50093237c7e948167b399734a7ece
Khyl Nad <khylanad@gmail.com>
2024-02-27 17:50:30 -0700

    completed start_up.xml, added app colors to themes.xml

--- app/src/main/java/com/example/nostack/MainActivity.java
+++ app/src/main/java/com/example/nostack/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_up);
 
         db = FirebaseFirestore.getInstance();
         userRef = db.collection("users");
