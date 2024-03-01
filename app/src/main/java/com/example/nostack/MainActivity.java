b2831081d918c064c2d88788cdcf828524b5ce74
Khyl Nad <khylanad@gmail.com>
2024-02-29 17:40:22 -0700

    added nav file, fragments for AttendeeEvent AttendeeHome

--- app/src/main/java/com/example/nostack/MainActivity.java
+++ app/src/main/java/com/example/nostack/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
         db = FirebaseFirestore.getInstance();
         userRef = db.collection("users");
