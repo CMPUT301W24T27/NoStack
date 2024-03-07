ef01f5b708e15101a678680b751ffe27a47de08c
Khyl Nad <khylanad@gmail.com>
2024-03-06 19:55:20 -0700

    moved QRScanning.java into utils

--- app/src/main/java/com/example/nostack/utils/QRScanning.java
+++ app/src/main/java/com/example/nostack/utils/QRScanning.java
@@ -1,4 +1,4 @@
-package com.example.nostack;
+package com.example.nostack.utils;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.nostack.R;
 
 /**
  * A simple {@link Fragment} subclass.
