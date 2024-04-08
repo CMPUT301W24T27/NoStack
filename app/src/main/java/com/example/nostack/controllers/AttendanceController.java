e35117a79babbb834ba2f7b3e0463c3e5e4ea417
natewu <36091727+natewu@users.noreply.github.com>
2024-04-08 15:39:59 -0600

    attendance javadoc

--- app/src/main/java/com/example/nostack/controllers/AttendanceController.java
+++ app/src/main/java/com/example/nostack/controllers/AttendanceController.java
@@ -14,10 +14,18 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+/**
+ * Class for handling attendance
+ */
 public class AttendanceController {
     private static AttendanceController singleInstance = null;
     private final CollectionReference attendanceCollectionReference = FirebaseFirestore.getInstance().collection("attendance");
     private final CurrentUserHandler currentUserHandler = CurrentUserHandler.getSingleton();
+
+    /**
+     * Get the instance of the AttendanceController
+     * @return void
+     */
     public static AttendanceController getInstance() {
         if (singleInstance == null) {
             singleInstance = new AttendanceController();
@@ -25,33 +33,71 @@ public class AttendanceController {
         return singleInstance;
     }
 
+    /**
+     * Empty public constructor
+     */
     public AttendanceController() {
     }
 
+    /**
+     * Get all attendance
+     * @return Task<QuerySnapshot> The attendance collection
+     */
     public Task<QuerySnapshot> getAllAttendance() {
         return attendanceCollectionReference.get();
     }
 
+    /**
+     * Get attendance by event
+     * @param eventId
+     * @return Task<QuerySnapshot> The attendance by event
+     */
     public Task<QuerySnapshot> getAttendanceByEvent(String eventId) {
         return attendanceCollectionReference.whereEqualTo("eventId", eventId).get();
     }
 
+    /**
+     * Get attendance by user
+     * @return Task<QuerySnapshot> The attendance by user
+     */
     public Task<QuerySnapshot>getAttendanceByUser() {
         return getAttendanceByUser(currentUserHandler.getCurrentUserId());
     }
 
+    /**
+     * Get attendance by user
+     * @param userId The user id
+     * @return Task<QuerySnapshot> The attendance by user
+     */
     public Task<QuerySnapshot> getAttendanceByUser(String userId) {
         return attendanceCollectionReference.whereEqualTo("userId", userId).get();
     }
 
+    /**
+     * Get attendance by id
+     * @param id The attendance id
+     * @return Task<DocumentSnapshot> The attendance by id
+     */
     public Task<DocumentSnapshot> getAttendanceById(String id) {
         return attendanceCollectionReference.document(id).get();
     }
 
+    /**
+     * Create attendance
+     * @param eventId The event id
+     * @return void
+     */
     public Task<Void> createAttendance(String eventId) {
         return createAttendance(currentUserHandler.getCurrentUserId(), eventId, null);
     }
 
+    /**
+     * Create attendance
+     * @param userId The user id
+     * @param eventId The event id
+     * @param location The location
+     * @return void
+     */
     public Task<Void> createAttendance(String userId, String eventId, @Nullable Location location) {
         Attendance newAtt = new Attendance(userId, eventId);
         if (location != null) {
@@ -61,6 +107,12 @@ public class AttendanceController {
         return attendanceCollectionReference.document(newAtt.getId()).set(newAtt);
     }
 
+    /**
+     * Check in attendance
+     * @param id The attendance id
+     * @param location The location
+     * @return void
+     */
     public Task<Void> attendanceCheckIn(String id, @Nullable Location location) {
         if (location != null) {
             GeoLocation latlng = new GeoLocation(location.getLatitude(), location.getLongitude());
@@ -69,15 +121,23 @@ public class AttendanceController {
         return attendanceCollectionReference.document(id).update("numCheckIn", FieldValue.increment(1));
     }
 
+    /**
+     * Delete attendance
+     * @param attendanceId The attendance id
+     * @return void
+     */
     public Task<Void> deleteAttendance(String attendanceId) {
         return attendanceCollectionReference.document(attendanceId).delete();
     }
 
+    /**
+     * Get present attendance
+     * @param eventId The event id
+     * @return Task<QuerySnapshot> The present attendance
+     */
     public Task<QuerySnapshot> getPresentAttendance(String eventId) {
         return attendanceCollectionReference
                 .whereEqualTo("eventId", eventId).whereGreaterThan("numCheckIn", 0)
                 .get();
     }
-
-
 }
